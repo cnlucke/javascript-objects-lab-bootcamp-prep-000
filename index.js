474883bc9fa17e39c430146b18e6705826396354
cnlucke <cristy.lucke@gmail.com>
2017-09-30 20:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return = Object.assign({}, object, { [key]: value })
 }
 
-//Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return = Object.assign(object, { [key]: value })
+}
+
+//Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
+function deleteFromObjectByKey(object, key) {
+  newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
 }
